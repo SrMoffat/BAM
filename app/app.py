7d0d27dd03286d7a67a4feacca40e92eb19d2469
Ngige Gitau <ngigemoffat@gmail.com>
2018-07-02 11:32:56 +0300

    Added login endpoint

--- app/app.py
+++ app/app.py
@@ -38,19 +38,30 @@ class SignUp(Resource):
                 'message' : 'Password too weak!'
             }, 422
 
+        hashed_password = generate_password_hash(password, method='sha256')
+
+        # Block double entry
+        for user in users:
+            if username == user['username']:
+                return {
+                    'status' : 409,
+                    'message' : 'Username already exists!'
+                }, 409
+
         # Create user object
         new_user = User(username=username,
-                        password=password,
+                        password=hashed_password,
                         admin=False)
         
         # Store the user object in user_holder
         new_user_holder = {}
         new_user_holder['username'] = username
+        new_user_holder['password'] = hashed_password
         new_user_holder['admin'] = new_user.admin
 
         # Check for successful instantiation then add new_user to storage
         if new_user:
-            users.append(new_user_holder)            
+            users.append(new_user_holder) 
         else:
             return jsonify({
                 'message' : 'User not created!'
@@ -60,3 +71,40 @@ class SignUp(Resource):
             'status' : 201,
             'message' : 'You have been succesfully registered!'
         }, 201
+
+class Login(Resource):
+    """
+    The resource for user login
+    """
+
+    def post(self):
+        """
+        The post method to login user POST api/v1/auth/login
+        """
+   
+        # Get input in json
+        username = request.json['username']
+        password = request.json['password']
+
+        # Check for null values for username and password
+        if not username or not password:
+            return {
+                'status': 400,
+                'message' : 'All fields required!'
+            },400
+
+        # Check provided credentials against storage
+        if username not in [user['username'] for user in users]:
+            return {
+                'status' : 403,
+                'message' : 'Invalid credentials!'
+            }, 403
+        for user in users:
+            if username == user['username'] and check_password_hash(user['password'], password):
+                return {
+                    'status' : 200,
+                    'message' : 'You are now logged in!'
+                }, 200
+
+
+      
--- app/endpoints.py
+++ app/endpoints.py
@@ -1,5 +1,6 @@
 from app.app import api
-from app.app import SignUp
+from app.app import SignUp, Login
 
 
 api.add_resource(SignUp, '/auth/signup')
+api.add_resource(Login, '/auth/login')
